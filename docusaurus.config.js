a8ecd700d0463d426bccfc9a5fdfa0a8e0af8c96
redhat123456 <1907065810@qq.com>
2021-11-25 09:04:35 +0800

    update yml

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,8 +13,8 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'redhat123456', // Usually your GitHub org/user name.
+  projectName: 'knowledge-web', // Usually your repo name.
 
   presets: [
     [
